b0433652c49c601e3a1b4f763d5198ec430c08d3
vivekananthan-gloify <vivekananthan@gloify.com>
2022-12-07 12:06:17 +0530

    KIAL 2 terminal added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
   //* For QA
   // **********************************************************
-  // baseUrl: 'https://hyd.carterx.in/index.php?',
-  // razorPayKey: 'rzp_test_fOTZDy0HmQGnS7',
+  baseUrl: 'https://hyd.carterx.in/index.php?',
+  razorPayKey: 'rzp_test_fOTZDy0HmQGnS7',
 
   //* For Production
   // **********************************************************
-  baseUrl: 'https://app.carterx.in/index.php?',
-  razorPayKey:'rzp_live_LthnWTU5SuA0Hg'
+  // baseUrl: 'https://app.carterx.in/index.php?',
+  // razorPayKey:'rzp_live_LthnWTU5SuA0Hg'
 };
